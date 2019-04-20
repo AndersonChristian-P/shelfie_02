3793f3365012d24cadab3bbf8e26ebf066cc4b1f
Christian Anderson <ca96187@gmail.com>
2019-04-20 14:26:24 -0600

    completed React

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 
+import Product from "../Product/Product"
+
 export default class Dashboard extends Component {
   constructor() {
     super()
@@ -11,7 +13,9 @@ export default class Dashboard extends Component {
 
   render() {
     return (
-      <div>Dashboard</div>
+      <div> Dashboard
+        <Product />
+      </div>
     )
   }
 
