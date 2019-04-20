8d197657a3d8fb5dc403a0791bc8bbaa96a6a46b
Christian Anderson <ca96187@gmail.com>
2019-04-20 17:56:46 -0600

    completed Part 1 Step 4

--- src/App.js
+++ src/App.js
@@ -5,28 +5,31 @@ import './App.css';
 import Header from "./Components/Header/Header"
 import Dashboard from "./Components/Dashboard/Dashboard"
 import Form from "./Components/Form/Form"
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      inventory: [
-        {
-          name: "shoes",
-          price: 5,
-          img: "https://assets.adidas.com/images/h_600,f_auto,q_auto:sensitive,fl_lossy/6a8f277daf5f4748a739a998016785f4_9366/Asweerun_Shoes_White_F36340_01_standard.jpg"
-        },
-        {
-          name: "socks",
-          price: 11,
-          img: "https://images.express.com/is/image/expressfashion/0006_01175481_0098?cache=on&wid=361&fmt=jpeg&qlt=75,1&resmode=sharp2&op_usm=1,1,5,0&defaultImage=Photo-Coming-Soon"
-        },
-        {
-          name: "shirts",
-          price: 23,
-          img: "https://gloimg.rglcdn.com/rosegal/pdm-product-pic/Clothing/2018/09/28/goods-img/1546624752690648515.jpg"
-        }]
+      inventory: []
     }
+  }
+
+  getInventory = () => {
+    axios.get("/api/inventory")
+      .then(res => {
+        this.setState({
+          inventory: res.data
+        })
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(`GET axios err ${err}`)
+      })
+  }
+
+  componentDidMount() {
+    this.getInventory()
   }
 
   render() {
